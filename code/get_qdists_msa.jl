33350c5679dcb7429c400c6d3471d1099e8021b9
gerhardJaeger <gerhard.jaeger@uni-tuebingen.de>
2024-06-05 08:54:49 +0200

    qdists recomputed

--- code/get_qdists_msa.jl
+++ code/get_qdists_msa.jl
@@ -33,6 +33,16 @@ function gqd_upgma(ds)
     return gqd
 end
 
+##
+
+function gqd_nj(ds)
+    msa_tree = "neighbour_joining_trees/$(ds)_nj.tre"
+    glot_tree = "../data/glottolog_trees/$(ds)_glottolog.tre"
+    output = read(`qdist $msa_tree $glot_tree`, String)
+    gqd = 1 - parse(Float64, split(output, "\t")[end-2])
+    return gqd
+end
+
 
 ##
 
@@ -62,42 +72,44 @@ for ds in datasets
     push!(results, (
             ds=ds,
             gqd_upgma=gqd_upgma(ds),
+            gqd_nj=gqd_nj(ds),
             gqd_cc=gqd_cc(ds),
             gqd_msa=gqd_msa(ds)
         )
     )
 end
 
-results_df = filter(x -> !isnan(x.gqd_upgma) && !isnan(x.gqd_cc) && !isnan(x.gqd_msa), DataFrame(results))
+results_df = filter(x -> !isnan(x.gqd_upgma) && !isnan(x.gqd_nj) && !isnan(x.gqd_cc) && !isnan(x.gqd_msa), DataFrame(results))
 
 ##
 
 @pipe results_df |>
-    stack(_, [:gqd_upgma, :gqd_cc, :gqd_msa]) |>
+    stack(_, [:gqd_upgma, :gqd_nj, :gqd_cc, :gqd_msa]) |>
     @df _ dotplot(:variable, :value, group=:variable, xlabel="Method", ylabel="QD", legend=false)
 
 ##
 
 @pipe results_df |>
-    stack(_, [:gqd_upgma, :gqd_cc, :gqd_msa]) |>
+    stack(_, [:gqd_upgma, :gqd_nj, :gqd_cc, :gqd_msa]) |>
     @df _ boxplot(:variable, :value, group=:variable, xlabel="Method", ylabel="QD", legend=false)
 @pipe results_df |>
-    stack(_, [:gqd_upgma, :gqd_cc, :gqd_msa]) |>
+    stack(_, [:gqd_upgma, :gqd_nj, :gqd_cc, :gqd_msa]) |>
     @df _ dotplot!(:variable, :value, group=:variable, xlabel="Method", ylabel="QD", legend=false)
 
 
 ##
 @pipe results_df |>
-    stack(_, [:gqd_upgma, :gqd_cc, :gqd_msa]) |>
+    stack(_, [:gqd_upgma, :gqd_nj, :gqd_cc, :gqd_msa]) |>
     groupby(_, :variable) |>
     combine(_, :value => mean => :mean, :value => std => :std)
 
 #     Row │ variable   mean      std      
 #     │ String     Float64   Float64  
 # ─────┼───────────────────────────────
-#   1 │ gqd_upgma  0.236889  0.17803
-#   2 │ gqd_cc     0.261024  0.174856
-#   3 │ gqd_msa    0.246258  0.143159
+#   1 │ gqd_upgma  0.249513  0.174721
+#   2 │ gqd_nj     0.208609  0.164862
+#   3 │ gqd_cc     0.211304  0.164284
+#   4 │ gqd_msa    0.236392  0.156864
 
 
 ##
@@ -105,15 +117,15 @@ results_df = filter(x -> !isnan(x.gqd_upgma) && !isnan(x.gqd_cc) && !isnan(x.gqd
 
 # Assuming df is your DataFrame and results_df is already defined
 # Extract min and max for the axes
-min_val = min(minimum(results_df.gqd_cc), minimum(results_df.gqd_msa))
-max_val = max(maximum(results_df.gqd_cc), maximum(results_df.gqd_msa))
+min_val = min(minimum(results_df.gqd_cc), minimum(results_df.gqd_nj))
+max_val = max(maximum(results_df.gqd_cc), maximum(results_df.gqd_nj))
 
 # Set plot dimensions to be square
 plot_width = 600
 plot_height = 600
 
 # Create the plot with manually set limits and aspect ratio 1:1
-@df results_df scatter(:gqd_cc, :gqd_msa, xlabel="QD CC", ylabel="QD MSA", legend=false, 
+@df results_df scatter(:gqd_cc, :gqd_nj, xlabel="QD CC", ylabel="QD NJ", legend=false, 
                        xlim=(min_val, max_val), ylim=(min_val, max_val), 
                        size=(plot_width, plot_height), aspect_ratio=1)
 
